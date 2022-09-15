c4811ae5c38f434a1eab10282854cd09d57d6ca6
wheatup <acphun@gmail.com>
2022-09-15 12:31:50 +0900

    add sass support

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 /*global chrome*/
-import logo from './logo.svg';
-import './App.css';
+import './scss/style.scss';
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Woah it's a React Chrome extension...</p>
-        <a
-          className='App-link'
-          href='https://developer.chrome.com/docs/extensions/mv3/getstarted/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          View Extension Dev Docs
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Is it working?</h1>
+		</div>
+	);
 }
 
 export default App;
